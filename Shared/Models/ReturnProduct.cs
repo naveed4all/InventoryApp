832d690da979b17b305685e1d293e808dd0606f6
naveedekram <naveed.ekram@thesoftcube.com>
2022-07-29 07:24:20 +0500

    returnproduct

--- Shared/Models/ReturnProduct.cs
+++ Shared/Models/ReturnProduct.cs
@@ -1,6 +1,28 @@
-﻿namespace Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
 {
     public class ReturnProduct
     {
+        [Key]
+        public int ReturnId { get; set; }
+        
+        public int ItemId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; } 
+
+        public string Reason { get; set; }
+
+        public bool IsDamaged { get; set; }
+
+        public double ItemPrice { get; set; }
+
+        public double Total { get; set; }
+
+        public string Bill_Number { get; set; }
+
+
     }
 }
